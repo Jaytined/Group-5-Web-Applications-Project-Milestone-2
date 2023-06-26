db7e27b4ba85f1a4f37f7bfc38e4684e463f7757
aeley320 <124070241+aeley320@users.noreply.github.com>
2023-06-26 14:16:47 -0400

    Update Home.js to use Header from Header.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Nav from "../Nav";
 import Footer from "../Footer";
+import Header from "../Header";
 
 
 function Home(){
@@ -9,16 +10,8 @@ function Home(){
 
 		<div>
 
-			<div className="banner">
-				<header className="text-center" role = "banner">
-					<h1><img className="image" src="images/mango.jpg" alt="Header image"/>
-						Group 5's Mango Weekly Planner
-						<img className="image" src="images/mango.jpg" alt="Header image"/>
-					</h1>
-					<p>This page should be tried in safari, chrome or Mozila.</p>
-				</header>
-			</div>
-
+                        <Header/>
+	    
 			<div className="login">
 				<form action="index.html">
 					
