83d86eae4129a418082a4eba12b42f44391aee5d
aeley320 <124070241+aeley320@users.noreply.github.com>
2023-06-26 14:08:49 -0400

    Updated Details.js to use Header from Header.js and corrected indentation issues

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Nav from "../Nav";
 import Footer from "../Footer";
+import Header from "../Header";
 
 
 function Details(){
@@ -9,51 +10,38 @@ function Details(){
 
 	<div>
 
-<       div className="banner">
-            <header className="text-center" role = "banner">
-                <h1><img className="image" src="images/mango.jpg" alt="Header image"/>
-                    Group 5's Mango Weekly Planner
-                    <img className="image" src="images/mango.jpg" alt="Header image"/>
-                </h1>
-                <p>This page should be tried in safari, chrome or Mozila.</p>
-            </header>
-		</div>
+            <Header/>
 
-        <div className="login">
-            <form action="index.html">
+            <div className="login">
+                <form action="index.html">
                 
-                <label htmlFor="email">E-mail: </label>
-                <input type="text" id="Email" name="email"></input>
+                    <label htmlFor="email">E-mail: </label>
+                    <input type="text" id="Email" name="email"></input>
+                    <input type="submit" value="Login"></input>
 
-                <input type="submit" value="Login"></input>
-
-            </form>
-        </div>
-
-
-        <Nav/>
-
-        
-		
-        
-    <article>
-        <section className="text-center viewDetail">
-            <h1>Viewing Event Details</h1>
-            <div className="viewEvent">
-                Event Time: 2:00pm <br/>
-                Event Name: Example Event 1<br/>
-                Event Description: Info/details given about specific event <br/>
-                Further Description: Viewing Specifc event details can display more information about the event <br/>
-                that is not shown in the Weekly Planner view of the user's events <br/>
-                Priority Event: Yes <br/>
+                </form>
             </div>
-        </section>
-    </article>
 
-    <Footer/>		
+            <Nav/>
 
-	
-    </div>
+        		     
+            <article>
+                <section className="text-center viewDetail">
+                    <h1>Viewing Event Details</h1>
+                    <div className="viewEvent">
+                        Event Time: 2:00pm <br/>
+                        Event Name: Example Event 1<br/>
+                        Event Description: Info/details given about specific event <br/>
+                        Further Description: Viewing Specifc event details can display more information about the event <br/>
+                        that is not shown in the Weekly Planner view of the user's events <br/>
+                        Priority Event: Yes <br/>
+                   </div>
+               </section>
+           </article>
+ 
+           <Footer/>		
+	    
+        </div>
     );
 
         
@@ -63,4 +51,3 @@ function Details(){
 
 
 export default Details;
-
