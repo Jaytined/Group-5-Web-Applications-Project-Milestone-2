899c556ac6e849e354366103484186a14e3ef1d0
aeley320 <124070241+aeley320@users.noreply.github.com>
2023-06-27 21:46:39 -0400

    Update Header.js to fix image rendering issue

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,13 @@
+mport React from "react";
 import './style.css'; 
+import mango from './mango.jpg';
 function Header(){
 	return(
         <div className="banner">
 				<header className="text-center" role = "banner">
-					<h1><img className="image" src="first-app/mango.jpg" alt="Header image"/>
+					<h1><img className="image" src={mango} alt="Header image"/>
 						Group 5's Mango Weekly Planner
-						<img className="image" src="./react-app/mango.jpg" alt="Header image"/>
+						<img className="image" src={mango} alt="Header image"/>
 					</h1>
 					<p>This page should be tried in safari, chrome or Mozila.</p>
 				</header>
