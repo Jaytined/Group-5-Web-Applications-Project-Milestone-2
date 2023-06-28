d6d1a2ebe12a2271fa51dc6dbe7c08077f1628a7
Jaytined <118459543+Jaytined@users.noreply.github.com>
2023-06-27 21:53:07 -0400

    Fixed header CSS merged images change

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,12 @@
-mport React from "react";
 import './style.css'; 
-import mango from './mango.jpg';
+import mango from './pages/images/mango.jpg'
+
 function Header(){
 	return(
         <div className="banner">
 				<header className="text-center" role = "banner">
 					<h1><img className="image" src={mango} alt="Header image"/>
-						Group 5's Mango Weekly Planner
+						Mango Weekly Planner
 						<img className="image" src={mango} alt="Header image"/>
 					</h1>
 					<p>This page should be tried in safari, chrome or Mozila.</p>
